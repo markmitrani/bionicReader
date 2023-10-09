52cd307975a8a370e3f0332e3b87aa4066dd76e4
Mark Mitrani <M.M.MitraniSabah@student.tudelft.nl>
2023-10-09 12:46:52 +0200

    add fix for the newlines stuck between 2 words

--- main.py
+++ main.py
@@ -24,18 +24,15 @@ if __name__ == '__main__':
 
     my_csv = pd.read_table('output.tsv')
 
+
+    # Replace \newline with actual newlines and \tab with actual tabs in the DataFrame
+    # subtask: replace \newline with space or empty string based on surrounding whitespace:
+    # first erase newlines surrounded by whitespace. then for the remaining, directly erase.
+    my_csv['para'] = my_csv['para'].str.replace(r'(\S)\\newline(\S)', r'\1\2').str.replace(r'\newline', ' ').str.replace(r'\tab', '')
+
+    #test print(my_csv['para'][8])
+
     my_csv['para_bold'] = my_csv['para']
     my_csv['para_bold'] = my_csv['para_bold'].apply(bionic_my_text)
-    """
-    newdoc = pd.DataFrame(columns=['item_id', 'para', 'para_bold', 'question', 'opt1', 'opt2', 'opt3', 'opt4', 'correct_answer'])
-    newdoc['item_id'] = my_csv.index
-    newdoc['para'] = my_csv['story']
-    newdoc['para_bold'] = my_csv['story_bolded']
-    newdoc['question'] = my_csv['question']
-
-    print(newdoc['item_id'])
-    print(my_csv['answer_options'].iloc[2])
-    print(type(ast.literal_eval(my_csv['answer_options'].iloc[2])))
-    """
     my_csv.to_csv('output_with_bolds.csv')
    # bionic_my_text(inpt)
